8068f621eb1d795fdfbec35b3a79f001aa764e52
jakemanger <jakesmanger@gmail.com>
2021-05-27 17:14:18 +0800

    some antialiasing changes

--- NEVE_unity_URP/Assets/QuickOutline/Scripts/Outline.cs
+++ NEVE_unity_URP/Assets/QuickOutline/Scripts/Outline.cs
@@ -2,7 +2,8 @@
 //  Outline.cs
 //  QuickOutline
 //
-//  Created by Chris Nolet on 3/30/18.
+//  Modified by Jake Manger during 2021.
+//  Originally Created by Chris Nolet on 3/30/18.
 //  Copyright © 2018 Chris Nolet. All rights reserved.
 //
 
